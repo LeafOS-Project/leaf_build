bca37041f13cd8e61145db2c5b5281a72f2e9457
Tim Zimmermann <tim@linux4.de>
2024-06-21 20:21:30 +0200

    jenkins: Import www publish pipeline
    Change-Id: I74770a272f9ae5c63103b4461b6b8233a9c7f9f8

---
+++ jenkins/www/leaf-www.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+    options {
+        checkoutToSubdirectory('jenkins/build')
+        disableConcurrentBuilds()
+    }
+    stages {
+        stage('Init'){
+            steps {
+                cleanWs()
+            }
+        }
+        stage('Push to production'){
+            steps {
+                sh '''#!/bin/bash
+                        set -e
+			mkdir -p build
+
+                        export MASTER_IP="$(echo $SSH_CLIENT | cut -f1 -d ' ')"
+                        ssh $MASTER_IP git -C /var/www/leafos.org/ git fetch refs/changes/${GERRIT_CHANGE_NUMBER: -2}/${GERRIT_CHANGE_NUMBER}/${GERRIT_PATCHSET_NUMBER}
+                        ssh $MASTER_IP git -C /var/www/leafos.org/ git reset --hard FETCH_HEAD
+                        ssh $MASTER_IP "cd /var/www/leafos.org; composer install"
+                '''
+            }
+        }
+    }
+    post {
+        always {
+            deleteDir()
+        }
+    }
+}
